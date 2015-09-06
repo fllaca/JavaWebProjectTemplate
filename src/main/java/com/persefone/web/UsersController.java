b7c099470f76ff082247177dd188c11bcc1b5e4a
fernando <ferllarom@gmail.com>
2015-09-06 21:46:37 +0200

    created users controllers

---
+++ src/main/java/com/persefone/web/UsersController.java
@@ -0,0 +1,24 @@
+package com.persefone.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+ 
+@Controller
+@RequestMapping("/users")
+public class UsersController {
+ 
+    @RequestMapping(method = RequestMethod.GET)
+    public String sayHello(ModelMap model) {
+        model.addAttribute("greeting", "Hello World from Spring 4 MVC");
+        return "users";
+    }
+ 
+    @RequestMapping(value = "/helloagain", method = RequestMethod.GET)
+    public String sayHelloAgain(ModelMap model) {
+        model.addAttribute("greeting", "Hello World Again, from Spring 4 MVC");
+        return "user";
+    }
+ 
+}
